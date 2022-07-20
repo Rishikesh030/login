90b03b62e9b79298223f9fb37bc5ecfc66cf6ece
Rishikesh030 <109223744+Rishikesh030@users.noreply.github.com>
2022-07-20 16:52:58 +0530

    Add files via upload

---
+++ followingpattern.c
@@ -0,0 +1,12 @@
+// write a programe to print  following pattern 
+// 1,8,27,64......10000000
+#include<stdio.h>
+void main()
+{
+    int first=1,second=8,third=27,fourth=64;
+    
+    printf("%d ",first);
+    printf("%d ",second);
+    printf("%d ",third);
+    printf("%d ",fourth);
+}
